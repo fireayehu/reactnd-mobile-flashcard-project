1ea3b8eb62a229248a13d7633e46446475901a84
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2020-06-06 15:10:39 +0300

    feat: Adding Icons in the bottom tab

--- src/navigation/BottomTabsNavigation.js
+++ src/navigation/BottomTabsNavigation.js
@@ -1,14 +1,47 @@
 import React from "react";
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { FontAwesome } from "@expo/vector-icons";
 import DecksScreen from "../screens/DecksScreeen";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Decks" component={DecksScreen} />
-    <Tab.Screen name="NewDeck" component={DecksScreen} />
+    <Tab.Screen
+      name="Decks"
+      component={DecksScreen}
+      options={{
+        tabBarLabel: "Decks",
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome
+            name={Platform.select({
+              ios: "home",
+              android: "home",
+            })}
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="NewDeck"
+      component={DecksScreen}
+      options={{
+        tabBarLabel: "New Deck",
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome
+            name={Platform.select({
+              ios: "plus",
+              android: "plus",
+            })}
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
